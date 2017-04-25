f3c51f0fb4ede30d726790bd7b605e51e8ddbbf5
Matthew Nicholls <matt.nicholls@coreproductions.com.au>
2017-04-25 18:47:51 +1000

    Add a method to retrieve all races in a season

--- gof1.go
+++ gof1.go
@@ -7,55 +7,55 @@ import (
 	"net/http"
 )
 
-const baseUrl = "http://ergast.com/api/f1/"
+const baseURL = "http://ergast.com/api/f1"
 
+// GetRacesInSeason returns details of all races in the season specified.  Results are not returned.
 func GetRacesInSeason(year int) []Race {
-	url := fmt.Sprintf("%s%v/schedule.json", baseUrl, year)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Println("NewRequest:", err)
-		return nil
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Do:", err)
-		return nil
-	}
-	defer resp.Body.Close()
-
-	var result F1
-
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		log.Println(err)
-	}
-
+	url := fmt.Sprintf("%s/%v/schedule.json", baseURL, year)
+	result := makeAPICall(url)
 	return result.MRData.RaceTable.Races
 }
 
 // GetRacesWithResults queries the Ergast api and returns the details of every completed race in the F1 season specified
 func GetRacesWithResults(year int) []Race {
-	url := fmt.Sprintf("%s%v/results.json", baseUrl, year)
+	url := fmt.Sprintf("%s/%v/results.json?limit=1000&offset=0", baseURL, year)
+	result := makeAPICall(url)
+	return result.MRData.RaceTable.Races
+}
+
+// GetRaceWithResults retrieves details and results of the race specified by year and number
+func GetRaceWithResults(year int, raceNumber int) Race {
+	url := fmt.Sprintf("%s/%v/%v/results.json?limit=1000&offset=0", baseURL, year, raceNumber)
+	result := makeAPICall(url)
+
+	var r Race
+	if len(result.MRData.RaceTable.Races) != 1 {
+		return r
+	}
+
+	return result.MRData.RaceTable.Races[0]
+}
+
+func makeAPICall(url string) F1 {
+	var result F1
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Println("NewRequest:", err)
-		return nil
+		return result
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("Do:", err)
-		return nil
+		return result
 	}
 	defer resp.Body.Close()
-
-	var result F1
 
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		log.Println(err)
 	}
 
-	return result.MRData.RaceTable.Races
+	return result
 }
