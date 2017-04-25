12b399a0d8d82df12ce4bf04da229c9af1dbd110
Matthew Nicholls <matt.nicholls@coreproductions.com.au>
2017-04-25 18:00:38 +1000

    Add call to pull schedule

--- gof1.go
+++ gof1.go
@@ -7,10 +7,36 @@ import (
 	"net/http"
 )
 
-// GetRaces queries the Ergast api and returns the details of every completed race in the F1 season specified
-func GetRaces(year int) []Race {
-	url := fmt.Sprintf("http://ergast.com/api/f1/%v/results.json?limit=1000&offset=0", year)
-	fmt.Println(url)
+const baseUrl = "http://ergast.com/api/f1/"
+
+func GetRacesInSeason(year int) []Race {
+	url := fmt.Sprintf("%s%v/schedule.json", baseUrl, year)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("NewRequest:", err)
+		return nil
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Do:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	var result F1
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Println(err)
+	}
+
+	return result.MRData.RaceTable.Races
+}
+
+// GetRacesWithResults queries the Ergast api and returns the details of every completed race in the F1 season specified
+func GetRacesWithResults(year int) []Race {
+	url := fmt.Sprintf("%s%v/results.json", baseUrl, year)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Println("NewRequest:", err)
